02dfb9ec1a6ee8d318bc557f08feea46e4e5634c
Arf <51786641+myaltithink@users.noreply.github.com>
2026-01-14 14:23:59 +0800

    fix: forgot to include this in previous commit

--- src/constants/mechanics/beginner.ts
+++ src/constants/mechanics/beginner.ts
@@ -92,7 +92,7 @@ const setA: QuizQuestion[] = [
         choices: [
             "narrative",
             "narrativ",
-            "narrative",
+            "narretive",
             "narrattive",
         ],
         choiceType: "alpha"
